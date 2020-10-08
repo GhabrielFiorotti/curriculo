58930bbc301dfaf362064d0c6506aa0073981ccb
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-08 20:17:19 -0300

    Adicionando module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
+
+app.use(cors())
+app.use(express.json());
 
 app.use(express.static('public'))
 router.get('/',function(req,res){
@@ -18,6 +21,6 @@ router.get('/curr2', function(req, res){
 });
 
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(PORT);
 
 console.log('Running at Port 3000');
